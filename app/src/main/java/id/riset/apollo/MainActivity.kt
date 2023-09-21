468df9359e89819e3619f2f3f4c168a28e603811
Fauzan Ghozi Mubarak <fauzanghozi56@gmail.com>
2023-09-21 14:25:04 +0700

    update animation bottom nav

--- app/src/main/java/id/riset/apollo/MainActivity.kt
+++ app/src/main/java/id/riset/apollo/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,13 +24,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ApolloTheme {
-                val viewModel = hiltViewModel<CountriesViewModel>()
-                val state by viewModel.state.collectAsState()
-                CountriesScreen(
-                    state = state,
-                    onSelectedCountry = viewModel::selectedCountry,
-                    onDismissCountryDialog = viewModel ::dismisCountryDialog
-                )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CountriesScreen()
+                }
             }
         }
     }
--- app/src/main/java/id/riset/apollo/ui/CountriesScreen.kt
+++ app/src/main/java/id/riset/apollo/ui/CountriesScreen.kt
@@ -1,7 +1,19 @@
 package id.riset.apollo.ui
 
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,54 +21,119 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FloatingActionButtonDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asComposeRenderEffect
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.hilt.navigation.compose.hiltViewModel
+import id.riset.apollo.R
 import id.riset.apollo.domain.model.DetailedCountry
 import id.riset.apollo.domain.model.SimpelCountry
 import id.riset.apollo.presentation.CountriesViewModel
+import kotlin.math.PI
+import kotlin.math.sin
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.filled.AcUnit
+import id.riset.apollo.utils.times
+import id.riset.apollo.utils.transform
+
+
+@RequiresApi(Build.VERSION_CODES.S)
+private fun getRenderEffect(): RenderEffect {
+    val blurEffect = RenderEffect
+        .createBlurEffect(80f, 80f, Shader.TileMode.MIRROR)
+
+    val alphaMatrix = RenderEffect.createColorFilterEffect(
+        ColorMatrixColorFilter(
+            ColorMatrix(
+                floatArrayOf(
+                    1f, 0f, 0f, 0f, 0f,
+                    0f, 1f, 0f, 0f, 0f,
+                    0f, 0f, 1f, 0f, 0f,
+                    0f, 0f, 0f, 50f, -5000f
+                )
+            )
+        )
+    )
+
+    return RenderEffect
+        .createChainEffect(alphaMatrix, blurEffect)
+}
 
 @Composable
 fun CountriesScreen(
-    state : CountriesViewModel.CountriesState,
-    onSelectedCountry : (code :String) -> Unit,
-    onDismissCountryDialog : () -> Unit
-){
-    Box(modifier = Modifier.fillMaxSize()){
-        if (state.isLoading){
+    state: CountriesViewModel.CountriesState,
+    onSelectedCountry: (code: String) -> Unit,
+    onDismissCountryDialog: () -> Unit,
+    renderEffect: androidx.compose.ui.graphics.RenderEffect?,
+    fabAnimationProgress: Float = 0f,
+    clickAnimationProgress: Float = 0f,
+    toggleAnimation: () -> Unit = { }
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        if (state.isLoading) {
             CircularProgressIndicator(
                 modifier = Modifier.align(Alignment.Center)
             )
-        }else{
+        } else {
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
-            ){
-              items(state.countries){country->
-                CountryItem(
-                    country = country,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            onSelectedCountry(country.code)
-                        }
-                        .padding(16.dp)
+            ) {
+                items(state.countries) { country ->
+                    CountryItem(
+                        country = country,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                onSelectedCountry(country.code)
+                            }
+                            .padding(16.dp)
 
-                )
-              }
+                    )
+                }
             }
-            if(state.selectedCountry != null){
+            if (state.selectedCountry != null) {
                 CountryDialog(
                     country = state.selectedCountry,
                     onDismiss = onDismissCountryDialog,
@@ -64,25 +141,79 @@ fun CountriesScreen(
                         .clip(RoundedCornerShape(5.dp))
                         .background(Color.White)
                         .padding(16.dp)
-                    )
+                )
             }
         }
+        CustomBottomNavigation()
+        Circle(
+            color = MaterialTheme.colors.primary.copy(alpha = 0.5f),
+            animationProgress = 0.5f
+        )
+        FabGroup(renderEffect = renderEffect, animationProgress = fabAnimationProgress)
+        FabGroup(
+            renderEffect = null,
+            animationProgress = fabAnimationProgress,
+            toggleAnimation = toggleAnimation
+        )
+        Circle(
+            color = Color.Cyan,
+            animationProgress = clickAnimationProgress
+        )
+    }
+}
+
+@Composable
+fun CountriesScreen() {
+    val isMenuExtended = remember { mutableStateOf(false) }
+
+    val fabAnimationProgress by animateFloatAsState(
+        targetValue = if (isMenuExtended.value) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 1000,
+            easing = LinearEasing
+        )
+    )
+
+    val clickAnimationProgress by animateFloatAsState(
+        targetValue = if (isMenuExtended.value) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = LinearEasing
+        )
+    )
+
+    val renderEffect = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        getRenderEffect().asComposeRenderEffect()
+    } else {
+        null
+    }
+    val viewModel = hiltViewModel<CountriesViewModel>()
+    val state by viewModel.state.collectAsState()
+    CountriesScreen(
+        state = state,
+        onSelectedCountry = viewModel::selectedCountry,
+        onDismissCountryDialog = viewModel::dismisCountryDialog,
+        renderEffect = renderEffect,
+        fabAnimationProgress = fabAnimationProgress,
+        clickAnimationProgress = clickAnimationProgress
+    ) {
+        isMenuExtended.value = isMenuExtended.value.not()
     }
 }
 
 @Composable
 private fun CountryItem(
-    country : SimpelCountry,
+    country: SimpelCountry,
     modifier: Modifier = Modifier
-){
-    Row (
+) {
+    Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
-    ){
+    ) {
         Text(
             text = country.emoji,
             fontSize = 30.sp
-            
+
         )
         Spacer(modifier = Modifier.width(16.dp))
         Column(
@@ -97,7 +228,7 @@ private fun CountryItem(
             Text(
                 text = country.capital,
 
-            )
+                )
         }
     }
 }
@@ -105,16 +236,16 @@ private fun CountryItem(
 @Composable
 private fun CountryDialog(
     country: DetailedCountry,
-    onDismiss:()->Unit,
+    onDismiss: () -> Unit,
     modifier: Modifier = Modifier
-){
+) {
     Dialog(onDismissRequest = onDismiss) {
-        Column (
+        Column(
             modifier = modifier
         ) {
-            Row (
+            Row(
                 modifier = Modifier.fillMaxWidth()
-            ){
+            ) {
                 Text(
                     text = country.emoji,
                     fontSize = 30.sp
@@ -142,3 +273,138 @@ private fun CountryDialog(
     }
 
 }
+
+@Composable
+fun Circle(color: Color, animationProgress: Float) {
+    val animationValue = sin(PI * animationProgress).toFloat()
+
+    Box(
+        modifier = Modifier
+            .padding(44.dp)
+            .size(56.dp)
+            .scale(2 - animationValue)
+            .border(
+                width = 2.dp,
+                color = color.copy(alpha = color.alpha * animationValue),
+                shape = CircleShape
+            )
+    )
+}
+
+@Composable
+fun CustomBottomNavigation() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .height(80.dp)
+            .paint(
+                painter = painterResource(R.drawable.bottom_navigation),
+                contentScale = ContentScale.FillHeight
+            )
+            .padding(horizontal = 40.dp)
+    ) {
+        listOf(Icons.Filled.DateRange, Icons.Filled.Settings).map { image ->
+            IconButton(onClick = { }) {
+                Icon(imageVector = image, contentDescription = null, tint = Color.White)
+            }
+        }
+    }
+}
+
+@Composable
+fun FabGroup(
+    animationProgress: Float = 0f,
+    renderEffect: androidx.compose.ui.graphics.RenderEffect? = null,
+    toggleAnimation: () -> Unit = { }
+) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .graphicsLayer { this.renderEffect = renderEffect }
+            .padding(bottom = 44.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+
+        AnimatedFab(
+            icon = Icons.Default.PhotoCamera,
+            modifier = Modifier.padding(
+                PaddingValues(
+                    bottom = 72.dp,
+                    end = 210.dp
+                ) * FastOutSlowInEasing.transform(0f, 0.8f, animationProgress)
+            ),
+            opacity = LinearEasing.transform(0.3f, 0.8f, animationProgress)
+        )
+
+        AnimatedFab(
+            icon = Icons.Default.AcUnit,
+            modifier = Modifier.padding(
+                PaddingValues(
+                    bottom = 88.dp,
+                ) * FastOutSlowInEasing.transform(0.1f, 0.9f, animationProgress)
+            ),
+            opacity = LinearEasing.transform(0.3f, 0.8f, animationProgress)
+        )
+
+        AnimatedFab(
+            icon = Icons.Default.Star,
+            modifier = Modifier.padding(
+                PaddingValues(
+                    bottom = 72.dp,
+                    start = 210.dp
+                ) * FastOutSlowInEasing.transform(0.2f, 1.0f, animationProgress)
+            ),
+            opacity = LinearEasing.transform(0.4f, 0.9f, animationProgress)
+        )
+
+//        AnimatedFab(
+//            modifier = Modifier
+//                .scale(1f - LinearEasing.transform(0.5f, 0.85f, animationProgress)),
+//        )
+
+        AnimatedFab(
+            icon = Icons.Default.Add,
+            modifier = Modifier
+                .rotate(
+                    225 * FastOutSlowInEasing
+                        .transform(0.35f, 0.65f, animationProgress)
+                ),
+            onClick = toggleAnimation,
+            backgroundColor = Color.Transparent
+        )
+    }
+}
+
+@Composable
+fun AnimatedFab(
+    modifier: Modifier,
+    icon: ImageVector? = null,
+    opacity: Float = 1f,
+    backgroundColor: Color = MaterialTheme.colors.secondary,
+    onClick: () -> Unit = {}
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        elevation = FloatingActionButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp),
+        backgroundColor = backgroundColor,
+        modifier = modifier.scale(1.25f)
+    ) {
+        icon?.let {
+            Icon(
+                imageVector = it,
+                contentDescription = null,
+                tint = Color.White.copy(alpha = opacity)
+            )
+        }
+    }
+}
+
+
+//@Composable
+//@Preview(device = "id:pixel_4a", showBackground = true, backgroundColor = 0xFF3A2F6E)
+//private fun MainScreenPreview() {
+//    FluidBottomNavigationTheme {
+//        MainScreen()
+//    }
+//}
